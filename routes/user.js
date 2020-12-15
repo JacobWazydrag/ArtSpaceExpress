0a71cf37b60cac68440823aa063df17da7be1f32
J-Waz <jgw.jakegeorge@gmail.com>
2020-12-15 04:41:35 -0600

    wrote sign in method, npm i jwtexpress, jwt

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup } = require('../controllers/user')
+const { signup, signin } = require('../controllers/user')
 const { userSignupValidator } = require('../validator/index');
 
 //when user hits the route, will hit validator first then signup controller
 router.post('/signup', userSignupValidator, signup);
+router.post('/sigin', signin);
 
 module.exports = router;
