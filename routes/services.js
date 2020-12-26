254fdb988acd7a5c0363d622e0e1e8edde0656b6
J-Waz <jgw.jakegeorge@gmail.com>
2020-12-26 12:44:46 -0600

    queries, and model updates

--- routes/services.js
+++ routes/services.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-
-const { create, read, serviceById, remove, update } = require('../controllers/services');
+const {
+    create,
+    read,
+    serviceById,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    img
+} = require('../controllers/services');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 //when user hits the route, will hit validator first then signup controller
 router.post('/services/create/:userId', requireSignin, isAuth, isAdmin, create);
-router.get('/services/:serviceId', read)
-router.delete('/services/:serviceId/:userId', requireSignin, isAuth, isAdmin, remove)
-router.put('/services/:serviceId/:userId', requireSignin, isAuth, isAdmin, update)
+router.get('/services/:serviceId', read);
+router.delete('/services/:serviceId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/services/:serviceId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/services', list);
+router.get('/services/related/:serviceId', listRelated);
+router.get('/service/categories', listCategories);
+router.post("/services/by/search", listBySearch);
+router.get('/services/img/:serviceId', img)
 
 router.param('userId', userById);
 router.param('serviceId', serviceById);
-
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { requireSignin, isAuth, isAdmin, isGM } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const { requireSignin, isAuth, isAdmin, isGM, isManager } = require('../controllers/auth');
+const { userById, read, update } = require('../controllers/user');
 
-router.get('/secret/:userId', requireSignin, isAuth, isGM, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
     });
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
 
 router.param('userId', userById);
 
