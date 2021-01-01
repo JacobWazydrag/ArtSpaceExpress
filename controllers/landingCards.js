bfdabdf473b6d2ad0e426de59f8d5e2f34e55f82
J-Waz <jgw.jakegeorge@gmail.com>
2021-01-01 08:42:00 -0600

    update landing cards done tested

--- controllers/landingCards.js
+++ controllers/landingCards.js
@@ -3,7 +3,6 @@ const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
 const { errorHandler } = require('../helpers/dbErrorHandler');
-const landingCards = require('../models/landingCards');
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
@@ -46,15 +45,70 @@ exports.create = (req, res) => {
     });
 };
 
+exports.landingCardById = (req, res, next, id) => {
+    LandingCard.findById(id).exec((err, landingCard) => {
+        if (err || !landingCard) {
+            return res.status(400).json({
+                error: 'Landing Card does not exist'
+            });
+        }
+        req.landingCard = landingCard;
+        next();
+    });
+};
+
 exports.list = (req, res) => {
     LandingCard.find()
         .select('-img')
-        .exec((err, services) => {
+        .exec((err, landingCards) => {
             if (err) {
                 return res.status(400).json({
-                    error: 'Services not found!'
+                    error: 'landingCards not found!'
                 });
             }
-            res.json(services);
+            res.json(landingCards);
         });
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+        //check for all fields
+        const { title, subTitle} = fields;
+        const { img } = files;
+
+        if (!title || !subTitle || !img) {
+            return res.status(400).json({
+                error: 'Please fill required fields'
+            });
+        }
+
+        let landingCard = req.landingCard;
+        landingCard = _.extend(landingCard, fields);
+
+        if (files.img) {
+            //restrict file size
+            if (files.img.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image must be smaller than 1mb'
+                });
+            }
+            landingCard.img.data = fs.readFileSync(files.img.path);
+            landingCard.img.contentType = files.img.type;
+        }
+        landingCard.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
 };
